d7fe2b8b11d2d536cebe54a6ffb050b995be2179
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2026-07-04 16:28:46 +0800

    Improve KsmlCodeDeclVisionProvider

--- src/main/kotlin/com/tttsaurus/ksml/language/navigation/usage/provider/KsmlCodeDeclVisionProvider.kt
+++ src/main/kotlin/com/tttsaurus/ksml/language/navigation/usage/provider/KsmlCodeDeclVisionProvider.kt
@@ -2,9 +2,9 @@ package com.tttsaurus.ksml.language.navigation.usage.provider
 
 import com.intellij.find.actions.ShowUsagesAction
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.ui.awt.RelativePoint
 import com.tttsaurus.ksml.grammar.psi.KsmlCodeDecl
 import com.tttsaurus.ksml.language.KsmlFile
 import com.tttsaurus.ksml.language.navigation.KsmlCodeVisionProviderBase
@@ -40,10 +40,23 @@ class KsmlCodeDeclVisionProvider : KsmlCodeVisionProviderBase() {
             !FunctionUsageEntrypoint.findReferencesViaFunctionUsage(codeDecl, it).isEmpty()
         }
 
-        return if (filtered.isEmpty()) {
-            null
+        if (filtered.isEmpty()) return null
+
+        var consistent = true
+        val firstUsage = filtered.first()
+        for (usage in filtered) {
+            if (usage.file.fileType.name != firstUsage.file.fileType.name) {
+                consistent = false
+                break;
+            }
+        }
+
+        if (consistent) {
+            return "${filtered.size} usages (${firstUsage.file.fileType.defaultExtension})"
         } else {
-            "${filtered.size} usages"
+            val ksmlUsages = filtered.filter { it.file.fileType.defaultExtension == "ksml" }
+            val glslUsages = filtered.filter { it.file.fileType.defaultExtension == "glsl" }
+            return "${ksmlUsages.size} usages (ksml) + ${glslUsages.size} usages (glsl)"
         }
     }
 
@@ -52,13 +65,19 @@ class KsmlCodeDeclVisionProvider : KsmlCodeVisionProviderBase() {
         element: PsiElement,
         event: MouseEvent?
     ) {
-        val popupPosition = JBPopupFactory
-            .getInstance()
-            .guessBestPopupLocation(editor)
+        val visual = editor.offsetToVisualPosition(element.textOffset)
+        val xy = editor.visualPositionToXY(visual)
+
+        xy.y += editor.lineHeight
+
+        val point = RelativePoint(
+            editor.contentComponent,
+            xy
+        )
 
         ShowUsagesAction.startFindUsages(
             element,
-            popupPosition,
+            point,
             editor
         )
     }
